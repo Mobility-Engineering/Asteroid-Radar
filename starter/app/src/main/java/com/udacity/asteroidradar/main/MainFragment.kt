2bce0fb90a19fe82718a5d4390ec135519c8ce3b
Daniel Vidales <daniel.vidales@dexcom.com>
2022-11-16 00:53:15 -0800

    DATA BINDING/VIEW MODEL. Initial data binding between MainFragment and ViewModel

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -1,24 +1,34 @@
 package com.udacity.asteroidradar.main
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.udacity.asteroidradar.R
+import com.udacity.asteroidradar.database.AsteroidDatabase
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
+import java.lang.reflect.Array.get
 
 class MainFragment : Fragment() {
 
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
-    }
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = FragmentMainBinding.inflate(inflater)
-        binding.lifecycleOwner = this
+        val application = requireNotNull(this.activity).application
+        val dataSource = AsteroidDatabase.getInstance(application).asteroidDao
+        val viewModelFactory = MainViewModelFactory(dataSource, application)
+            viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+
 
         binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        //binding.statusLoadingWheel.visibility =
 
         setHasOptionsMenu(true)
 
