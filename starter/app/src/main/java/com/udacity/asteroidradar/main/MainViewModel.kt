d3fdb2e67fc99f84b7cf5ac9c50eb8a75b83532e
Daniel Vidales <daniel.vidales@dexcom.com>
2022-11-16 00:56:42 -0800

    VIEW MODEL. ViewModel and ViewModelFactory definition to pass existing database for its initialization

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,55 @@
 package com.udacity.asteroidradar.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.view.View
+import androidx.lifecycle.*
+import com.udacity.asteroidradar.bindTextViewToDisplayVelocity
+import com.udacity.asteroidradar.database.AsteroidDao
+import com.udacity.asteroidradar.database.AsteroidEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MainViewModel : ViewModel() {
-}
+class MainViewModel(
+    val database: AsteroidDao,
+    application: Application
+) : AndroidViewModel(application) {
+
+     val today: String = "YYYY-MM-DD"
+     var onProgress = false
+    private val asteroids = database.getAllAsteroids()
+    private val todayAsteroids = database.getTodayAsteroids(today)
+    //From where can today be obtained in the specified format
+
+     val testText = "test text"
+     val progressVisibility = View.GONE
+
+
+
+
+    private fun insert(asteroid: AsteroidEntity){
+        viewModelScope.launch{
+            insertAsteroid(asteroid)
+        }
+    }
+    suspend fun insertAsteroid(asteroid:AsteroidEntity){
+        withContext(Dispatchers.IO){
+            database.insert(asteroid)
+        }
+    }
+
+
+    fun someWorkToBeDone(){
+        viewModelScope.launch {
+            suspendFunction()
+        }
+    }
+
+        suspend fun suspendFunction(){
+            withContext(Dispatchers.IO){
+                //Do long running work here i.e get all asteroids/insert all asteroids
+            }
+        }
+
+    }
+
